b0440ef96c42d5f029bf0ac6c22bb18d54e593b8
nilton-clemente123 <nilton.clemente@tecsup.edu.pe>
2026-09-23 21:43:31 -0500

    crear estructura inicial de TECSUP Fit

---
+++ app/src/main/java/com/clemente/tecsupfit/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.clemente.tecsupfit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.clemente.tecsupfit.components.BottomBar
+import com.clemente.tecsupfit.ui.theme.TecsupFitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TecsupFitTheme {
+                TECSUPFitApp()
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TECSUPFitApp() {
+
+    Scaffold(
+
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("TECSUP Fit")
+                }
+            )
+        },
+
+        bottomBar = {
+            BottomBar()
+        }
+
+    ) { innerPadding ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Text(
+                text = "Inicio"
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/clemente/tecsupfit/components/BottomBar.kt
@@ -0,0 +1,77 @@
+package com.clemente.tecsupfit.components
+
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Event
+import androidx.compose.material.icons.filled.FitnessCenter
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.RadioButtonUnchecked
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BottomBar() {
+
+    NavigationBar {
+
+        NavigationBarItem(
+            selected = true,
+            onClick = { },
+            icon = {
+                Icon(
+                    imageVector = Icons.Outlined.RadioButtonUnchecked,
+                    contentDescription = "Inicio"
+                )
+            },
+            label = {
+                Text("Inicio")
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = {
+                Icon(
+                    imageVector = Icons.Outlined.RadioButtonUnchecked,
+                    contentDescription = "Reservas"
+                )
+            },
+            label = {
+                Text("Reservas")
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = {
+                Icon(
+                    imageVector = Icons.Outlined.RadioButtonUnchecked,
+                    contentDescription = "Rutinas"
+                )
+            },
+            label = {
+                Text("Rutinas")
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = {
+                Icon(
+                    imageVector = Icons.Outlined.RadioButtonUnchecked,
+                    contentDescription = "Perfil"
+                )
+            },
+            label = {
+                Text("Perfil")
+            }
+        )
+    }
+}
